e0d2cd4b235b012113ed6856ea1a05c890cd6107
Williams Alex <williamsalexjr@gmail.com>
2021-09-02 09:50:58 +0100

    Made a few tweek and updated the module version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = (values) => {
   for (let i = 0; i < values.length; i++) {
     // Check for empty fields
     if (!values[i].field) {
-      return `${values[i].value ? `${values[i].value} value requires a field name` : 'A field name is required for your entries'}, i.e username, email, phone number e.t.c`;
+      return `${values[i].value ? `${values[i].value} value requires a field name` : 'A field name is required for your entry'}, i.e username, email, phone number e.t.c`;
     }
 
     // Check for empty values
@@ -12,12 +12,12 @@ module.exports = (values) => {
 
     // Check for value type text
     if (values[i].type == "text" && typeof values[i].type == String) {
-      return `${values[i].field} only requires text based characters`;
+      return `${values[i].field ? `${values[i].field} only requires text based characters` : 'A field in your entry requires text based input only'}`;
     }
 
     // Check for value type number
     if (values[i].type == "number" && typeof values[i].type == Number) {
-      return `${values[i].field} only requires number based characters`;
+      return `${values[i].field ? `${values[i].field} only requires number based characters` : 'A field in your entry requires number based input only'}`;
     }
 
     // Check for value type email
@@ -25,7 +25,7 @@ module.exports = (values) => {
       const emailRegX =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (!emailRegX.test(values[i].value)) {
-        return `${values[i].field} field ${values[i].value} is not valid, try a valid ${values[i].field} field`;
+        return `${values[i].field ? `${values[i].field} field ${values[i].value} is not valid, try a valid ${values[i].field} field`:'Invalid email entry'}`;
       }
     }
 
